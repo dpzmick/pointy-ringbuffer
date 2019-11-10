3661d75620e5860e0bff16f395afed4ffef7d1da
David Zmick <dpzmick@gmail.com>
2019-11-10 16:53:07 -0600

    stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+An attempt to understand rust's Pin/Unpin behavior by writing a bad (and
+probably incorrect ringbuffer)
--- ringbuffer.rs
+++ ringbuffer.rs
@@ -1,10 +1,17 @@
+#[derive(Debug)]
+enum RBErr {
+    Full,
+    Empty,
+}
+
+// can't do the struct allocation with trailing data thing here, I think?
 struct RingBuffer<T: Sized + Default> {
-    entries: [T; 4],           // FIXME use const_generics for this, or heap allocate (ironically heap allocting solves the entire problem)
-    head:    Option<*const T>, // technically never need to modify the data
+    entries: [T; 3],           // FIXME use const_generics for this, or heap allocate (ironically heap allocting solves the entire problem), size of 2 right now
+    head:    Option<*mut T>,   // technically never need to modify the data but more types is a pain
     tail:    Option<*mut T>,   // will write here
 }
 
-impl<T: Sized + Default> RingBuffer<T, 3> { // apparently these can't be const generic yet?
+impl<T: Sized + Default> RingBuffer<T> { // apparently these can't be const generic yet?
     fn new() -> Self {
         Self {
             entries: Default::default(),
@@ -15,8 +22,80 @@ impl<T: Sized + Default> RingBuffer<T, 3> { // apparently these can't be const g
             tail:    None,
         }
     }
+
+    // this is a hack! after putting in final resting place, init the
+    // thing
+    fn initptr(&mut self) {
+        self.head = Some(self.entries.as_mut_ptr());
+        self.tail = Some( unsafe {
+            self.entries.as_mut_ptr().add(1)
+        });
+    }
+
+    unsafe fn inc(&mut self, ptr: *mut T) -> *mut T {
+        if ptr == self.entries.as_mut_ptr().add(3 /* FIXME hardcoded constant */) {
+            return self.entries.as_mut_ptr();
+        }
+        else {
+            return ptr.add(1);
+        }
+    }
+
+    fn full(&mut self) -> bool {
+        if self.head == self.tail { return true; }
+        else                      { return false; }
+    }
+
+    fn empty(&mut self) -> bool {
+        if unsafe { self.inc(self.head.unwrap()) } == self.tail.unwrap() { return true; }
+        else                                                             { return false; }
+    }
+
+    fn push(&mut self, v: T) -> Result<(), RBErr> {
+        if self.full() { return Err(RBErr::Full); }
+        unsafe { *(self.tail.unwrap()) = v }; // move
+
+        self.tail = unsafe { Some(self.inc(self.tail.unwrap())) };
+        Ok( () )
+    }
+
+    fn pop(&mut self) -> Result<T, RBErr> {
+        if self.empty() { return Err(RBErr::Empty); }
+        let ret = unsafe { std::ptr::read(self.head.unwrap().add(1)) };
+        self.head = unsafe { Some(self.inc(self.head.unwrap())) };
+        return Ok( ret );
+    }
+}
+
+fn make() -> RingBuffer<u32> {
+    let mut r = RingBuffer::<u32>::new();
+    r.initptr(); // assume it won't move after this
+    r.push(10).unwrap();
+    r.push(11).unwrap();
+    return r; // uh oh
 }
 
 fn main() {
-    let r = RingBuffer::<u32, 2>::new();
+    let mut r = RingBuffer::<u32>::new();
+    r.initptr(); // assume it won't move after this
+    r.push(10).unwrap();
+    r.push(11).unwrap();
+    println!("{}", r.pop().unwrap());
+
+    // now, here's the problem.
+    // if we move this struct, we're screwed. without recalling
+    // initptr, there's nothing we can do to fix our pointers.
+    // since references are more-or-less the same thing as pointers,
+    // we have the same problem with references.
+
+    let mut r2 = r; // MOVE!
+    println!("{}", r2.pop().unwrap()); // this one actually works because we're taking the same stack slot
+
+    // this will not work, since r was created on stack frame of make
+    let mut r = make();
+    println!("{}", r.pop().unwrap()); // prints the right thing, but valgrind has a tantrum
+
+    // mem::replace and mem::swap both have potential to copy internal
+    // pointers around, screwing up the data stuctures. If we mark
+    // RingBuffer as !Unpin, I think that solves this problem.
 }
